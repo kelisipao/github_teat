fef281016770ae2fd7218dbf7c4d1dfab87c6775
jluxu <15948372192@163.com>
2017-07-11 09:40:57 +0800

    Update first_1.java

--- github_test/src/first/first_1.java
+++ github_test/src/first/first_1.java
@@ -2,7 +2,8 @@ package first;
 
 public class first_1 {
 	public static void main (String[] args) {
-	System.out.println("��һ�β���");
+	System.out.println("µÚÒ»´Î²âÊÔ");
+	System.out.println("I love basketball");
 	}
 
 }
